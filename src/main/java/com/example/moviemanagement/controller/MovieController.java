302c62c19ec5b327056ccf5faa179444e2cbd6f8
JagTheFriend <JagTheFriend12@gmail.com>
2026-06-28 10:04:26 +0530

    Add MovieController with GET /movie and POST /movie endpoints

---
+++ src/main/java/com/example/moviemanagement/controller/MovieController.java
@@ -0,0 +1,34 @@
+package com.example.moviemanagement.controller;
+
+import com.example.moviemanagement.model.Movie;
+import com.example.moviemanagement.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movie")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+
+    @PostMapping
+    public Movie addMovie(@RequestBody Movie movie) {
+        return movieService.addMovie(movie);
+    }
+}
